9dfe612f2887f1da2c4f50877c1f9b028d31a383
Doodles3 <eduardomoraes84@yahoo.com.br>
2020-11-07 00:55:24 -0300

    Better window initilization

--- MainGUI.cpp
+++ MainGUI.cpp
@@ -3,7 +3,9 @@
 // Init a frame without a parent frame
 // Init with any frame ID
 // Also set the title of the frame
-MainGUI::MainGUI() : wxFrame(nullptr, wxID_ANY, "Minesweeper")
+// Make the default starting location 30x30
+// Make the default size 800x600
+MainGUI::MainGUI() : wxFrame(nullptr, wxID_ANY, "Minesweeper", wxPoint(30, 30), wxSize(800, 600))
 {
 }
 
