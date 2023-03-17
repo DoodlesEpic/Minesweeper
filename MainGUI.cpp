41c47a2339fc9c3f4d17410fd8c08c7064e022bb
Doodles <moraes.eduardo@proton.me>
2023-03-17 16:01:44 -0300

    refactor: reorder and remove most comments
    The previous comments were trully overdone and generally cluttered the
    code. I'll add comments back again after this refactoring round is done,
    hopefully there won't be a need for so many anymore.

--- MainGUI.cpp
+++ MainGUI.cpp
@@ -1,8 +1,5 @@
 ﻿#include "MainGUI.h"
 
-// The event table maps events to handler functions
-// The ids here and where we build the components must be equal
-
 // Disable clang-format because it doesn't know how to ident these macros
 // clang-format off
 wxBEGIN_EVENT_TABLE(MainGUI, wxFrame) EVT_MENU(20001, MainGUI::EasyDifficulty)
@@ -11,11 +8,6 @@ EVT_MENU(20003, MainGUI::HardDifficulty)
 EVT_MENU(20004, MainGUI::NewGame)
 EVT_MENU(20005, MainGUI::CloseGame) wxEND_EVENT_TABLE()
 
-// Init a frame without a parent frame
-// Init with any frame ID
-// Also set the title of the frame
-// Make the default starting location 30x30
-// Make the default size 800x600
 MainGUI::MainGUI() : wxFrame(nullptr, wxID_ANY, "Minesweeper", wxPoint(30, 30), wxSize(800, 600)) {
   // clang-format on
   // Reenable clang-format as it's not a problem to format the following code
@@ -46,51 +38,31 @@ MainGUI::MainGUI() : wxFrame(nullptr, wxID_ANY, "Minesweeper", wxPoint(30, 30), 
 }
 
 MainGUI::~MainGUI() {
-  // Delete the buttons and fieldMines array to prevent memleaks
-  // We don't need to handle the buttons themselves because wx does it for us
   delete[] buttons;
   delete[] fieldMines;
 }
 
 // Handles the click of any button in the minesweeper grid
 void MainGUI::OnButtonClicked(wxCommandEvent &event) {
-  // Find the coordinates of the clicked button
   const int buttonX = (event.GetId() - 10000) % fieldWidth;
   const int buttonY = (event.GetId() - 10000) / fieldHeight;
-
-  // Get the button index from the 2d grid to the 1d array
   const int buttonIndex = buttonY * fieldWidth + buttonX;
-
-  // Prevent this button from being clicked again
   buttons[buttonIndex]->Enable(false);
-
-  // Track how many squares we have clicked already
   ++clickedSquares;
 
-  // On the first click, generate the field mines
   if (isFirstClick) {
-    // For each mine there is on the field
     for (int minesPlanted = 0; minesPlanted < mines;) {
-      // Find the coordinates of the current mine
       const int mineX = rand() % fieldWidth;
       const int mineY = rand() % fieldHeight;
-
-      // Get the mine index from the 2d grid to the 1d array
       const int mineIndex = mineY * fieldWidth + mineX;
 
-      // Make sure there is no mine there yet
-      // Also make sure the first button will not be a mine
       if (fieldMines[mineIndex] == Mine::Empty && mineX != buttonX &&
           mineY != buttonY) {
-        // Plant a mine at the location
         fieldMines[mineIndex] = Mine::Planted;
-
-        // Count the mine planted
         ++minesPlanted;
       }
     }
 
-    // Make sure we don't generate the mine field again
     isFirstClick = false;
   }
 
@@ -101,24 +73,18 @@ void MainGUI::OnButtonClicked(wxCommandEvent &event) {
     GameOverReset();
   }
 
-  // There's no mine planted, count the surrounding mines and show the game win
-  // screen
   else {
-    // Count neighbour mines to display the label
     int neighbourMines = 0;
     for (int i = -1; i < 2; i++) {
       for (int j = -1; j < 2; j++) {
-        // Guarante this is not an out of bounds index
         const bool isValidIndex = buttonX + i >= 0 &&
                                   buttonX + i < fieldWidth &&
                                   buttonY + j >= 0 && buttonY + j < fieldHeight;
 
         if (isValidIndex) {
-          // Get the mine index from the 2d grid to the 1d array
           const int neighbourMineIndex =
               (buttonY + j) * fieldWidth + (buttonX + i);
 
-          // If there's a bomb planted at the location, count neighbour mines
           if (fieldMines[neighbourMineIndex] == Mine::Planted) {
             ++neighbourMines;
           }
@@ -126,12 +92,10 @@ void MainGUI::OnButtonClicked(wxCommandEvent &event) {
       }
     }
 
-    // If there are any neighbour mines, update the label count
     if (neighbourMines > 0) {
       buttons[buttonIndex]->SetLabel(std::to_string(neighbourMines));
     }
 
-    // Check for the winning game condition
     if (clickedSquares == (fieldWidth * fieldHeight) - mines) {
       DisplayBombsLocation();
       wxMessageBox("CHERNOBYL WAS AVOIDABLE, CONGRATULATIONS",
@@ -144,11 +108,8 @@ void MainGUI::OnButtonClicked(wxCommandEvent &event) {
 }
 
 void MainGUI::OnButtonRightClicked(wxMouseEvent &event) {
-  // Find the coordinates of the clicked button
   const int buttonX = (event.GetId() - 10000) % fieldWidth;
   const int buttonY = (event.GetId() - 10000) / fieldHeight;
-
-  // Get the button index from the 2d grid to the 1d array
   const int buttonIndex = buttonY * fieldWidth + buttonX;
 
   if (buttons[buttonIndex]->GetLabel() == "") {
@@ -158,11 +119,9 @@ void MainGUI::OnButtonRightClicked(wxMouseEvent &event) {
   }
 }
 
-// Display where the bombs where planted
 void MainGUI::DisplayBombsLocation() {
   for (int i = 0; i < fieldWidth; i++) {
     for (int j = 0; j < fieldHeight; j++) {
-      // Get the mine index from the 2d grid to the 1d array
       const int mineIndex = j * fieldWidth + i;
 
       if (fieldMines[mineIndex] == Mine::Planted) {
@@ -172,23 +131,14 @@ void MainGUI::DisplayBombsLocation() {
   }
 }
 
-// Resets the game field and first click variable
 void MainGUI::GameOverReset() {
-  // Reset the game
   isFirstClick = true;
   clickedSquares = 0;
 
-  // We only clear the field, but don't regenerate it
-  // This is because the next click will trigger the field generation
   for (int i = 0; i < fieldWidth; i++) {
     for (int j = 0; j < fieldHeight; j++) {
-      // Get the grid index from the 2d grid to the 1d array
       const int index = j * fieldWidth + i;
-
-      // Remove any planted mines
       fieldMines[index] = Mine::Empty;
-
-      // Reset the label and enable the button
       buttons[index]->SetLabel("");
       buttons[index]->Enable(true);
     }
@@ -197,33 +147,24 @@ void MainGUI::GameOverReset() {
 
 void MainGUI::GenerateNewField(int newFieldWidth, int newFieldHeight,
                                int newMines) {
-  // Change the difficulty
   fieldWidth = newFieldWidth;
   fieldHeight = newFieldHeight;
   mines = newMines;
 
-  // Remove all buttons and ajust columns and rows
   buttonGrid->Clear(true);
   buttonGrid->SetCols(fieldWidth);
   buttonGrid->SetRows(fieldHeight);
 
-  // Create the new grid of buttons
   buttons = new wxButton *[fieldWidth * fieldHeight];
-
-  // Initialize the array for the mines states too
   fieldMines = new Mine[fieldWidth * fieldHeight];
 
-  // Create a font to use on the buttons
   wxFont font(16, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD,
               false);
 
-  // Dinamically generate the minesweep field
   for (int i = 0; i < fieldWidth; i++) {
     for (int j = 0; j < fieldHeight; j++) {
-      // Get the button index from the 2d grid to the 1d array
       const int buttonIndex = j * fieldWidth + i;
 
-      // Create a new button and bind an event handler to the clicked event
       buttons[buttonIndex] = new wxButton(this, 10000 + buttonIndex);
       buttons[buttonIndex]->SetFont(font);
       buttons[buttonIndex]->Bind(wxEVT_COMMAND_BUTTON_CLICKED,
@@ -231,40 +172,27 @@ void MainGUI::GenerateNewField(int newFieldWidth, int newFieldHeight,
       buttons[buttonIndex]->Connect(
           wxEVT_RIGHT_DOWN, wxMouseEventHandler(MainGUI::OnButtonRightClicked),
           NULL, this);
-
-      // Initialize the default mine state to empty
-      fieldMines[buttonIndex] = Mine::Empty;
-
-      // Put the new button inside the button grid
       buttonGrid->Add(buttons[buttonIndex], 1, wxEXPAND | wxALL);
+
+      fieldMines[buttonIndex] = Mine::Empty;
     }
   }
 
-  // Use the sizer and update the layout
   buttonGrid->Layout();
 }
 
 void MainGUI::EasyDifficulty(wxCommandEvent &event) {
-  // Reset the game
   GameOverReset();
-
-  // Generate new field with correct difficulty
   GenerateNewField(5, 5, 6);
 }
 
 void MainGUI::MediumDifficulty(wxCommandEvent &event) {
-  // Reset the game
   GameOverReset();
-
-  // Generate new field with correct difficulty
   GenerateNewField(10, 10, 30);
 }
 
 void MainGUI::HardDifficulty(wxCommandEvent &event) {
-  // Reset the game
   GameOverReset();
-
-  // Generate new field with correct difficulty
   GenerateNewField(15, 15, 80);
 }
 
--- MainGUI.h
+++ MainGUI.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "wx/wx.h"
 
-// Possible states of the mines in the field
 enum class Mine { Planted, Empty };
 
 class MainGUI : public wxFrame {
@@ -10,49 +9,29 @@ public:
   ~MainGUI();
 
 protected:
-  // Width and height for the minesweeper grid
+  bool isFirstClick = true;
   int fieldWidth = 10;
   int fieldHeight = 10;
-
-  // Numbers of mines to be displaced on the field
   int mines = 30;
-
-  // Track how many squares were clicked so we can display the winning screen
   int clickedSquares = 0;
 
-  // 1D Array of pointers to buttons on the grid, and the grid sizer
   wxButton **buttons;
   wxGridSizer *buttonGrid;
+  wxMenuBar *menuBar;
+  Mine *fieldMines;
 
-  // Array of mines to store the state of each of the mines
-  Mine *fieldMines = nullptr;
+  wxDECLARE_EVENT_TABLE();
 
-  // Remeber if this is the first click so there's no fail in the first click
-  bool isFirstClick = true;
-
-  // Handles the click of any button in the minesweeper grid
   void OnButtonClicked(wxCommandEvent &event);
   void OnButtonRightClicked(wxMouseEvent &event);
 
-  // Declare the event table so it can handle the button click
-  wxDECLARE_EVENT_TABLE();
-
-  // Functions used in the game over situation
+  void GenerateNewField(int newFieldWidth, int newFieldHeight, int newMines);
   void DisplayBombsLocation();
   void GameOverReset();
 
-  // Used to generate a new field (on game start, and difficulty change)
-  void GenerateNewField(int newFieldWidth, int newFieldHeight, int newMines);
-
-  // The top menu bar
-  wxMenuBar *menuBar = nullptr;
-
-  // Difficulty event handlers
   void EasyDifficulty(wxCommandEvent &event);
   void MediumDifficulty(wxCommandEvent &event);
   void HardDifficulty(wxCommandEvent &event);
-
-  // Game menu event handlers
   void NewGame(wxCommandEvent &event);
   void CloseGame(wxCommandEvent &event);
 };
